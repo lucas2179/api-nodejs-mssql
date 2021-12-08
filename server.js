23b5ea1cbb69fe043f1d6d487c95f1951551dbe8
Renato Camara <camarabox@gmail.com>
2021-12-08 15:02:43 -0300

    Update server.js
    updating...

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const app = require('./config/express')();
 const port = app.get('port');
 
-// RODANDO NOSSA APLICAÇÃO NA PORTA SETADA
 app.listen(port, () => {
   console.log(`Server ON running on Port ${port}`)
 });
